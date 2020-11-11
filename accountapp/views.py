11563f2a8e5fca1544eab3cfe68896f7e112f311
Park HS <parkhyongsok@naver.com>
2020-11-12 04:19:16 +0900

    djust Django course 8 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -6,4 +6,4 @@ from django.shortcuts import render
 
 
 def hello_world(request):
-    return HttpResponse('안녕하세요')
+    return render(request, 'base.html')
--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -67,7 +67,7 @@ ROOT_URLCONF = 'pragmatic.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
