3ca8e2ff4bfbcba5b9f4b663948a37e6b9b796bf
Park HS <parkhyongsok@naver.com>
2020-11-12 20:44:51 +0900

    django course 11 commit

--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -131,3 +131,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
