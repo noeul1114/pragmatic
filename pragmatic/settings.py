6aa664a75740b0dc6ca0105e0d9c30dd6038e6df
Park HS <parkhyongsok@naver.com>
2020-11-20 17:35:01 +0900

    django course 29 commit

--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -143,3 +143,8 @@ STATICFILES_DIRS = [
 
 LOGIN_REDIRECT_URL = reverse_lazy('accountapp:hello_world')
 LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login')
+
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
