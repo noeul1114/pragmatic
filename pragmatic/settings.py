29bae6fb3e46e5df504fe44839b76849f239515f
Park HS <parkhyongsok@naver.com>
2020-11-24 17:17:35 +0900

    django course 39 commit

--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -39,7 +39,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- static/js/magicgrid.js
+++ static/js/magicgrid.js
@@ -277,7 +277,7 @@ MagicGrid.prototype.listen = function listen () {
 let magicGrid = new MagicGrid({
   container: '.container',
   animate: true,
-  gutter: 30,
+  gutter: 12,
   static: true,
   useMin: true
 });
