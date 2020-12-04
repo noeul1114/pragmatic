bc5eea3a8fe07e049fd92677f5a09bb743522436
Park HS <parkhyongsok@naver.com>
2020-12-05 00:34:45 +0900

    Hello_world reverse url deleted

--- accountapp/views.py
+++ accountapp/views.py
@@ -21,7 +21,7 @@ has_ownership = [account_ownership_required, login_required]
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
+    success_url = reverse_lazy('home')
     template_name = 'accountapp/create.html'
 
 
@@ -43,7 +43,7 @@ class AccountUpdateView(UpdateView):
     model = User
     context_object_name = 'target_user'
     form_class = AccountUpdateForm
-    success_url = reverse_lazy('accountapp:hello_world')
+    success_url = reverse_lazy('home')
     template_name = 'accountapp/update.html'
 
 
